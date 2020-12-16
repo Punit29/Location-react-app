1d8c5e3ce1581f4e569d5f171804bb01584882ba
Punit29 <44257688+Punit29@users.noreply.github.com>
2020-12-16 11:24:48 +0530

    loc

---
+++ src/Demo.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+const Demo = () => {
+    const [details, setDetails] = useState(null);
+
+    const getUserGeolocationDetails = () => {
+        fetch(
+            "https://geolocation-db.com/json/1a811210-241d-11eb-b7a9-293dae7a95e1"
+        )
+            .then(response => response.json())
+            .then(data => setDetails(data));
+    };
+
+    return (
+        <>
+            <header title="Get user IP and location in ReactJS" />
+
+            <div className="row">
+                <div className="col text-center">
+                    <h2>Find my IP and Location</h2>
+                    <p className="mt-3">
+                        <button
+                            className="btn btn-primary"
+                            onClick={getUserGeolocationDetails}
+                        >
+                            Find my details
+                        </button>
+
+                        <div className="row justify-content-center mt-3">
+                            <div className="col-lg-6 text-center text-dark">
+                                {details && (
+                                    <ul className="list-group">
+                                        <li className="list-group-item">
+                                            Location :{" "}
+                                            {`${details.city}, ${details.country_name}(${details.country_code})`}
+                                        </li>
+                                        <li className="list-group-item">
+                                            IP: {details.IPv4}
+                                        </li>
+                                    </ul>
+                                )}
+                            </div>
+                        </div>
+                    </p>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Demo;
